6f5b9829d1fe7a752efc9bafd42202f7d3f97b90
AngelFoxJP <32850359+Joemybird@users.noreply.github.com>
2024-04-22 12:14:53 -0500

    Add files via upload

---
+++ minecopy/README.txt
@@ -0,0 +1,28 @@
+Kazagistar's Computercraft Minecopy V2
+==========================================
+Ever get annoyed that you can't transfer your programs to a server that has HTTP disabled? Do you get annoyed when you have to type the same stuff into multiple computers? Want to code with a REAL text editor, and play SMP too? The solution is here!
+
+Just open your minecraft and log in to a computercraft shell, then run the script with a list of files as parameters. Sit back and relax as it reads in the files, compresses them automagically, and rapidly types them into your minecraft window, saving them.
+
+Notes: This program is buggy as hell. Make sure you call it from the same directory as the .lua files you want to transfer, because it will try to preserve relative file locations. If the text comes out with mistakes, slow down the keystrokes. If you want to be able to debug your code, disable compression. THE PROGRAM WILL RAPIDLY VOMIT KEYSTROKES INTO WHATEVER APPLICATION HAS FOCUS! IF YOU HAVE ANYTHING OTHER THEN MINECRAFT FOCUSED, IT WILL GET HAMMERED WITH LUA COMMANDS! YOU HAVE BEEN WARNED!
+
+Enjoy, and please send me any questions you have, or patches you make to the code!
+
+
+Linux requirements:
+- Python 2.7
+- libxdo
+- lua [OPTIONAL: needed for lua file compression]
+
+No links, this shit is trivial to install with a package manager on any distro. Once you have all the prerequesites, you should be able to just run the script.
+
+
+Windows requirements:
+- Python 2.7
+    http://www.python.org/download/
+- pywin32 [OPTIONAL: needed for automatic window switching]
+    http://sourceforge.net/projects/pywin32/files/
+- lua [OPTIONAL: needed for lua file compression]
+    http://code.google.com/p/luaforwindows/
+
+Windows is a little stupid when it comes to command line stuff like python. Go to My Computer>Properties>Advanced>Environment Variables, edit the PATH variable, and add to the end of it ";C:\Python27". Then you should be able to run the script doing something like: "> python scriptfolder/minecopy/minecopy.py luafolder/script1.lua luafolder/script2.lua"
---
+++ minecopy/minecopy.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+import time,sys,os,platform,subprocess
+
+###############################################
+#           Configurable options              #
+###############################################
+#time between keystrokes
+key_press_delay = 0.01
+#time for editor commands
+editor_wait_delay = 0.1
+#name of window to autoswap to if able
+window_name = "Minecraft"
+# removes .lua from filenames
+crop_endings = True 
+# how long to delay start if autoswap fails
+switch_delay = 10
+# compress if lua is installed
+try_compress = True
+###############################################
+
+# location of lua compression script
+lua_path = os.path.join(os.path.dirname(__file__),"app_maximum.lua")
+#location of temporary compressed file
+temp_path = os.path.join(os.path.dirname(__file__),"TEMPORARY_LUA_FILE")
+
+if platform.system() == "Linux":
+    # Import libxdo, for key output
+    from ctypes import *
+    xdo=cdll.LoadLibrary("libxdo.so.2")
+    xdo_context=xdo.xdo_new(None)
+     
+    def typeout(string):
+        xdo.xdo_type(xdo_context, 0, string, key_press_delay*1000000) # microseconds
+
+    def savefile():
+        typekeys(["ctrl","Return","ctrl","Left","Return"])
+
+    def typekeys(keysyms): # separation might be useful eventually?
+        for key in keysyms:
+            xdo.xdo_keysequence(xdo_context, 0, key, key_press_delay*1000000) # microseconds
+            time.sleep(editor_wait_delay)
+        
+if platform.system() == "Windows":
+    def windowswitch():
+        # Load the stuff for writing strings using com
+        from win32com import client
+        shell = client.Dispatch("WScript.Shell")
+        shell.AppActivate('Minecraft') 
+        
+    import SendKeysCtypes as key
+    
+    def typeout(string):
+        newstring = ""
+        for char in string:
+            if char in ["{","}","[","]","+","^","%","~","(",")"]:
+                char = "{%s}"%char
+            newstring += char
+        key.SendKeys(newstring, key_press_delay, True, True, True)
+    
+    def savefile():
+        sequence = ("{VK_CONTROL}{ENTER}{VK_CONTROL}{RIGHT}{ENTER}")
+        key.SendKeys(sequence, editor_wait_delay)
+
+def loadfile(filename):
+    global try_compress
+    if try_compress:
+        try:
+            subprocess.call(["lua", lua_path, "-o", temp_path, "--quiet", "--maximum", os.path.join(os.getcwd(),filename)])
+            filename = temp_path
+        except:  
+            try_compress = False
+    f = open(filename)
+    data = f.read()
+    f.close()
+    if try_compress:
+        os.remove(temp_path)
+    return data
+        
+def copyfile(filename):
+    data = loadfile(filename)
+    if crop_endings:
+        filename = filename.replace(".lua","")
+    typeout("rm "+filename+"\nedit "+filename+"\n")
+    time.sleep(editor_wait_delay) # make sure it has time to finish
+    typeout(data)
+    savefile()
+    
+if __name__ == "__main__":
+    try:
+        windowswitch()
+    except:
+        print "Select minecraft window manually and make sure a computercraft terminal is open!"
+        print "You have %d seconds or bad things will happen!!!" % switch_delay
+        time.sleep(switch_delay)
+    for filename in sys.argv[1:]:
+        copyfile(filename)
